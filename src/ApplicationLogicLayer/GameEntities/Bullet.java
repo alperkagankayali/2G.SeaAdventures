6a75a8316f08e952ee15e3857160e93b356defa6
Kasymbek <t.kas9mbek@gmail.com>
2018-04-19 19:05:22 +0300

    GameEntities v0.2
    Fixed bugs, Constructors remade, Added new images

---
+++ src/ApplicationLogicLayer/GameEntities/Bullet.java
@@ -0,0 +1,63 @@
+package GameEntities;
+
+import javafx.scene.image.Image;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Bullet extends GameObject {
+    private final String ID1_IMAGE = System.getProperty("user.dir") +  "images\\Yellow_projec_L.png";
+    private final String ID2_IMAGE = System.getProperty("user.dir") +  "images\\Blue_projectile_R.png";
+    private final String ID3_IMAGE = System.getProperty("user.dir") +  "images\\projectile_red_circle.png";
+
+    private double damage;
+    private int ID; // 1 for submarine bullets
+                    // 2 for enemy bullets
+                    // 3 for enemy skills
+
+    // ID constructor
+    public Bullet(double xPos, double yPos, double damage, int ID) throws FileNotFoundException {
+        super(xPos, yPos);
+
+        setID(ID);
+        setDamage(damage);
+
+        if( ID == 1){
+
+            setVelocity(450, 0);
+            setSpriteImage( new Image(new FileInputStream(ID1_IMAGE)));
+        }
+        else if( ID == 2){
+
+            setVelocity(-300, 0);
+            setSpriteImage( new Image(new FileInputStream(ID2_IMAGE)));
+        }
+        else if( ID == 3){
+
+            setVelocity(-500, 0);
+            setSpriteImage( new Image(new FileInputStream(ID3_IMAGE)));
+        }
+    }
+
+    public double getDamage() {
+        return damage;
+    }
+
+    public void setDamage(double damage) {
+        if( damage >= 0)
+            this.damage = damage;
+        else
+            throw new ArrayIndexOutOfBoundsException("Invalid value is entered");
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        if( ID == 1 || ID == 2 || ID ==3)
+            this.ID = ID;
+        else
+            throw new ArrayIndexOutOfBoundsException("Invalid value is entered");
+    }
+}
---
+++ src/ApplicationLogicLayer/GameEntities/SkillManager.java
@@ -0,0 +1,40 @@
+package GameEntities;
+
+import java.util.ArrayList;
+
+public class SkillManager {
+
+//Attributes
+private int subLevel;
+private ArrayList<Skill> skills; 
+
+private int basiccooldownTime;
+private int basicdamage;
+private int basictimeOfEffect;
+private Health basichealth;
+private double basicfinalAttackSpeed;
+
+//Constructor
+public SkillManager(int subLevel) {
+	this.subLevel = subLevel;
+}
+	
+//Methods
+public void update(Submarine s ) {
+	if(s.level != subLevel){
+	subLevel = s.level;
+}
+}
+public ArrayList<Skill> getSkills(int subLevel){
+	return skills;
+}
+public Skill getSkill1() {
+	return skills.get(0);
+}
+public Skill getSkill2() {
+	return skills.get(1);
+}
+public Skill getSkill3() {
+	return skills.get(2);
+}	
+}
