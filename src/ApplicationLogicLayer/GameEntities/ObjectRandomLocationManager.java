2408873f21bf3b173dfb8a9b7c8d3a8342de2312
Kasymbek <t.kas9mbek@gmail.com>
2018-04-19 19:26:54 +0300

    GameEntities v0.3

--- src/ApplicationLogicLayer/GameEntities/ObjectRandomLocationManager.java
+++ src/ApplicationLogicLayer/GameEntities/ObjectRandomLocationManager.java
@@ -2,56 +2,70 @@ package GameEntities;
 
 import java.util.Random;
 
-import javax.tools.JavaFileManager.Location;
-
 //cannot find the import for Location object
-
 public class ObjectRandomLocationManager {
 
-//Attributes
-private double x;
-private double y;
-private Location loc;
+	//Attributes
+	private double x;
+	private double y;
+	private Location loc;
 
-//for getting random. how big our screen is
-private double rangeMinX;
-private double rangeMinY;
+	//for getting random. how big our screen is
+	private double rangeMinX = 700;
+	private double rangeMinY = 0;
 
-private double rangeMaxX;
-private double rangeMaxY;
+	private double rangeMaxX = 14000;
+	private double rangeMaxY = 480;
 
-//Constructor
-public ObjectRandomLocationManager() {
-	//Generate location.
-	Location loc = this.generateLocation();
-	this.loc = loc;
-}	
+	//Constructor
+	public ObjectRandomLocationManager() {
+		//Generate location.
+		Location loc = this.generateLocation();
+		this.loc = loc;
+	}
 
-//Methods
-public Location generateLocation() {
-	Random rand = new Random();
-	
-	double x = rangeMinX + (rangeMaxX - rangeMinX) * rand.nextDouble();
-	double y = rangeMinY + (rangeMaxY - rangeMinY) * rand.nextDouble();
-	
-	this.x = x;
-	this.y = y;
-	
-	Location newLocation = new Location(x,y);
-	return newLocation;
-}
+	//Methods
+	public Location generateLocation() {
+		Random rand = new Random();
 
-public double getX() {
-	return x;
-}
+		double x = rangeMinX + (rangeMaxX - rangeMinX) * rand.nextDouble();
+		double y = rangeMinY + (rangeMaxY - rangeMinY) * rand.nextDouble();
 
-public double getY() {
-	return y;
-}
+		this.x = x;
+		this.y = y;
 
-public Location getLoc() {
-	return loc;
-}
+		Location newLocation = new Location(x,y);
+		return newLocation;
+	}
 
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public Location getLoc() {
+		return loc;
+	}
+
+	private class Location {
+		double x;
+		double y;
+
+		Location( double x, double y){
+			this.x = x;
+			this.y = y;
+		}
+
+		public double getX() {
+			return x;
+		}
+
+		public double getY() {
+			return y;
+		}
+	}
 }
 
