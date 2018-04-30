40041e99e50ccb72c5689b74010326d60eec673c
salihzeki <salihzekiokur@gmail.com>
2018-04-30 12:53:40 +0300

    Add files via upload

--- src/SettingManager.java
+++ src/SettingManager.java
@@ -1,20 +1,23 @@
+import javafx.scene.input.KeyCode;
+
 /**
  * Created by Okur on 07/03/18.
  */
 
 
-public class SettingManager {
+public class SettingManager /*implements Runnable*/ {
     SoundEngine soundEngine;
+   //InputManager inputManager;
 
     //Integers that hold key ID's
-    public static int UP_KEY = 0;
-    public static int DOWN_KEY = 1;
-    public static int LEFT_KEY = 2;
-    public static int RIGHT_KEY = 3;
-    public static int SHOOTING_KEY = 4;
-    public static int SKILL1_KEY = 5;
-    public static int SKILL2_KEY = 6;
-    public static int SKILL3_KEY = 7;
+    public static KeyCode UP_KEY = KeyCode.UP;
+    public static KeyCode DOWN_KEY = KeyCode.DOWN;
+    public static KeyCode LEFT_KEY = KeyCode.LEFT;
+    public static KeyCode RIGHT_KEY = KeyCode.RIGHT;
+    public static KeyCode SHOOTING_KEY = KeyCode.SPACE;
+    public static KeyCode SKILL1_KEY = KeyCode.Z;
+    public static KeyCode SKILL2_KEY = KeyCode.X;
+    public static KeyCode SKILL3_KEY = KeyCode.C;
 
     SettingManager(SoundEngine soundEngine) {
         this.soundEngine = soundEngine;
@@ -41,4 +44,80 @@ public class SettingManager {
     public double getMusicVolume() { return soundEngine.getMusicVolume(); }
 
     public void muteUnMuteMusic(){ soundEngine.pauseUnPauseMusic(); }
+
+    public void changeControls() {
+        //new Thread(new InputManager()).start();
+
+        KeyCode k = KeyCode.K;
+        KeyCode newK = KeyCode.A;
+        int i = 0;
+
+        while (InputManager.isKeyReleased()){
+            try
+                {Thread.sleep(0);}
+            catch (Exception e)
+                {e.printStackTrace();}
+        }
+        while (k == KeyCode.K){ k = InputManager.getPressedKey();}
+        while (!InputManager.isKeyReleased()){
+            try
+                {Thread.sleep(0);}
+            catch (Exception e)
+                {e.printStackTrace();}
+        }
+        while (InputManager.isKeyReleased()){
+            try
+                {Thread.sleep(0);}
+            catch (Exception e)
+                {e.printStackTrace();}
+        }
+        while (newK == KeyCode.A){newK = InputManager.getPressedKey();}
+
+        if (k == this.UP_KEY) this.UP_KEY = newK;
+        else if (k == this.DOWN_KEY) changeDownKey(newK);
+        else if (k == this.LEFT_KEY) changeLeftKey(newK);
+        else if (k == this.RIGHT_KEY) changeRightKey(newK);
+        else if (k == this.SHOOTING_KEY) changeShootKey(newK);
+        else if (k == this.SKILL1_KEY) changeSkill1Key(newK);
+        else if (k == this.SKILL2_KEY) changeSkill2Key(newK);
+        else if (k == this.SKILL3_KEY) changeSkill3Key(newK);
+    }
+
+    public void changeUpKey(KeyCode k){
+        this.UP_KEY = k;
+    }
+
+    public void changeDownKey(KeyCode k){
+        this.DOWN_KEY = k;
+    }
+
+    public void changeLeftKey(KeyCode k){
+        this.LEFT_KEY = k;
+    }
+
+    public void changeRightKey(KeyCode k){
+        this.RIGHT_KEY = k;
+    }
+
+    public void changeShootKey(KeyCode k){
+        this.SHOOTING_KEY = k;
+    }
+
+    public void changeSkill1Key(KeyCode k){
+        this.SKILL1_KEY = k;
+    }
+
+    public void changeSkill2Key(KeyCode k){
+        this.SKILL2_KEY = k;
+    }
+
+    public void changeSkill3Key(KeyCode k){
+        this.SKILL3_KEY = k;
+    }
+    /*
+    @Override
+    public void run() {
+        changeControls();
+    }
+    */
 }
--- src/SettingsMenuPane.java
+++ src/SettingsMenuPane.java
@@ -1,11 +1,14 @@
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 
 /**
  * Created by Okur on 13/03/18.
@@ -13,24 +16,112 @@ import javafx.scene.layout.*;
 public class SettingsMenuPane {
 
     //private Image backgroundImage = new Image (SettingsMenuPane.class.getResource("Settings_Background.bmp").toString());
+    private String UpKey;
+    private String DownKey;
+    private String LeftKey;
+    private String RightKey;
+    private String ShootKey;
+    private String Skill1Key;
+    private String Skill2Key;
+    private String Skill3Key;
     GridPane gridPaneS;
 
 
     SettingsMenuPane(SettingManager settingManager){
 
+        UpKey = settingManager.UP_KEY.getName();
+        DownKey = settingManager.DOWN_KEY.getName();
+        LeftKey = settingManager.LEFT_KEY.getName();
+        RightKey = settingManager.RIGHT_KEY.getName();
+        ShootKey = settingManager.SHOOTING_KEY.getName();
+        Skill1Key = settingManager.SKILL1_KEY.getName();
+        Skill2Key = settingManager.SKILL2_KEY.getName();
+        Skill3Key = settingManager.SKILL3_KEY.getName();
 
+        Text UpText = new Text(UpKey);
+        Text DownText = new Text(DownKey);
+        Text LeftText = new Text(LeftKey);
+        Text RightText = new Text(RightKey);
+        Text ShootingText = new Text(ShootKey);
+        Text Skill1Text = new Text(Skill1Key);
+        Text Skill2Text = new Text(Skill2Key);
+        Text Skill3Text = new Text(Skill3Key);
 
         final Button button0 = new Button("Change Music");
         //final Button button1 = new Button("Change Up Key");
+        final Button button1 = new Button("Change Controls");
         final Slider slider0 = new Slider(0,1,settingManager.getMusicVolume());
         final Slider slider1 = new Slider(0,1,settingManager.getFXVolume());
+
 
         button0.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) { settingManager.changeBackGroundMusic(); }
         });
+        /*
+        button1.setOnAction(new EventHandler<ActionEvent>() {
 
+            @Override
+            public void handle(ActionEvent event) {
+                new Thread(settingManager).start();
+
+                UpKey = settingManager.UP_KEY.getName();
+                DownKey = settingManager.DOWN_KEY.getName();
+                LeftKey = settingManager.LEFT_KEY.getName();
+                RightKey = settingManager.RIGHT_KEY.getName();
+                ShootKey = settingManager.SHOOTING_KEY.getName();
+                Skill1Key = settingManager.SKILL1_KEY.getName();
+                Skill2Key = settingManager.SKILL2_KEY.getName();
+                Skill3Key = settingManager.SKILL3_KEY.getName();
+
+                UpText.setText(DownKey);
+                DownText.setText(DownKey);
+                LeftText.setText(LeftKey);
+                RightText.setText(RightKey);
+                ShootingText.setText(ShootKey);
+                Skill1Text.setText(Skill1Key);
+                Skill2Text.setText(Skill2Key);
+                Skill3Text.setText(Skill3Key);
+
+            }
+        });
+        */
+        button1.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+
+                Task task = new Task() {
+                    protected Integer call() {
+                        settingManager.changeControls();
+
+                        UpKey = settingManager.UP_KEY.getName();
+                        DownKey = settingManager.DOWN_KEY.getName();
+                        LeftKey = settingManager.LEFT_KEY.getName();
+                        RightKey = settingManager.RIGHT_KEY.getName();
+                        ShootKey = settingManager.SHOOTING_KEY.getName();
+                        Skill1Key = settingManager.SKILL1_KEY.getName();
+                        Skill2Key = settingManager.SKILL2_KEY.getName();
+                        Skill3Key = settingManager.SKILL3_KEY.getName();
+
+                        UpText.setText(UpKey);
+                        DownText.setText(DownKey);
+                        LeftText.setText(LeftKey);
+                        RightText.setText(RightKey);
+                        ShootingText.setText(ShootKey);
+                        Skill1Text.setText(Skill1Key);
+                        Skill2Text.setText(Skill2Key);
+                        Skill3Text.setText(Skill3Key);
+
+
+
+                        return 3;
+                    }
+                };
+
+                new Thread(task).start();
+            }
+        });
 
         slider0.valueProperty().addListener(new ChangeListener<Number> (){
             public void changed(ObservableValue<? extends Number> ov,
@@ -39,6 +130,8 @@ public class SettingsMenuPane {
 
             }
         } );
+
+
 
         slider1.valueProperty().addListener(new ChangeListener<Number> (){
             public void changed(ObservableValue<? extends Number> ov,
@@ -54,6 +147,15 @@ public class SettingsMenuPane {
         gridPaneS.add(button0,2,2);
         gridPaneS.add(slider0,2,3);
         gridPaneS.add(slider1,2,4);
+        gridPaneS.add(button1,2,5);
+        gridPaneS.add(UpText,2,6);
+        gridPaneS.add(DownText,2,7);
+        gridPaneS.add(LeftText,2,8);
+        gridPaneS.add(RightText,2,9);
+        gridPaneS.add(ShootingText,2,10);
+        gridPaneS.add(Skill1Text,2,11);
+        gridPaneS.add(Skill2Text,2,12);
+        gridPaneS.add(Skill3Text,2,13);
         //gridPaneS.add(button1,2,5);
 
         final Image backgroundImage = new Image (SettingsMenuPane.class.getResource("Settings_Background.bmp").toString());
