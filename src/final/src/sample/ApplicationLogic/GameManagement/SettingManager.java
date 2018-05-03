eb7b484195fde0abe5d7d2d86d0d82d81aa6ab42
salihzeki <salihzekiokur@gmail.com>
2018-05-03 19:28:13 +0300

    Add files via upload

--- src/final/src/sample/ApplicationLogic/GameManagement/SettingManager.java
+++ src/final/src/sample/ApplicationLogic/GameManagement/SettingManager.java
@@ -1,39 +1,136 @@
-package sample.ApplicationLogic.GameManagement;
+import javafx.scene.input.KeyCode;
 
-import java.awt.*;
-import java.awt.event.KeyEvent;
+/**
+ * Created by Okur on 07/03/18.
+ */
+
 
 public class SettingManager {
-    private int pressedKey;
-    private boolean isKeyReleased;
+    SoundEngine soundEngine;
 
-    public boolean isKeyReleased() {
-        return isKeyReleased;
-    }
-    public SettingManager(){
+    //Integers that hold key ID's
+    public static KeyCode UP_KEY = KeyCode.UP;
+    public static KeyCode DOWN_KEY = KeyCode.DOWN;
+    public static KeyCode LEFT_KEY = KeyCode.LEFT;
+    public static KeyCode RIGHT_KEY = KeyCode.RIGHT;
+    public static KeyCode SHOOTING_KEY = KeyCode.SPACE;
+    public static KeyCode SKILL1_KEY = KeyCode.Z;
+    public static KeyCode SKILL2_KEY = KeyCode.X;
+    public static KeyCode SKILL3_KEY = KeyCode.C;
 
+    SettingManager(SoundEngine soundEngine) {
+        this.soundEngine = soundEngine;
     }
-    public int getPressedKey(){
-        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
 
-            @Override
-            public boolean dispatchKeyEvent(KeyEvent ke) {
-                synchronized (SettingManager.class) {
-                    switch (ke.getID()) {
-                        case KeyEvent.KEY_PRESSED:
-                            pressedKey = ke.getKeyCode();
-                            isKeyReleased = false;
-                            break;
-                        case KeyEvent.KEY_RELEASED:
-                            pressedKey = 0;
-                            isKeyReleased = true;
-                            break;
-                    }
-                    return false;
-                }
-            }
-        });
-        //return new KeyEvent(this).getKeyCode();
-        return pressedKey;
+    public void changeBackGroundMusic(){
+        soundEngine.changeMusic();
     }
+
+    public void changeFXVolume(double volume){
+        soundEngine.changeFXVolume(volume);
+    }
+
+    public void changeMusicVolume(double volume){ soundEngine.changeMusicVolume(volume); }
+
+    public double getFXVolume() { return soundEngine.getFXVolume(); }
+
+    public double getMusicVolume() { return soundEngine.getMusicVolume(); }
+
+    public void muteUnMuteMusic(){ soundEngine.pauseUnPauseMusic(); }
+
+    /*
+    public void changeControls() {
+
+        KeyCode k = null;
+        KeyCode newK = null;
+
+
+
+        while (k == null){ k = InputManager.getPressedKey();}
+
+        while (!InputManager.isKeyReleased()){
+            try
+                {Thread.sleep(0);}
+            catch (Exception e)
+                {e.printStackTrace();}
+        }
+
+
+        while (InputManager.isKeyReleased()){
+            try
+                {Thread.sleep(0);}
+            catch (Exception e)
+                {e.printStackTrace();}
+        }
+
+
+        while (newK == null){newK = InputManager.getPressedKey();}
+
+        if (k == this.UP_KEY) this.UP_KEY = newK;
+        else if (k == this.DOWN_KEY) changeDownKey(newK);
+        else if (k == this.LEFT_KEY) changeLeftKey(newK);
+        else if (k == this.RIGHT_KEY) changeRightKey(newK);
+        else if (k == this.SHOOTING_KEY) changeShootKey(newK);
+        else if (k == this.SKILL1_KEY) changeSkill1Key(newK);
+        else if (k == this.SKILL2_KEY) changeSkill2Key(newK);
+        else if (k == this.SKILL3_KEY) changeSkill3Key(newK);
+    }
+    */
+
+    public void changeUpKey(){
+        KeyCode k = null;
+        while (k == null){ k = InputManager.getPressedKey();}
+
+        this.UP_KEY = k;
+    }
+
+    public void changeDownKey(){
+        KeyCode k = null;
+        while (k == null){ k = InputManager.getPressedKey();}
+
+        this.DOWN_KEY = k;
+    }
+
+    public void changeLeftKey(){
+        KeyCode k = null;
+        while (k == null){ k = InputManager.getPressedKey();}
+
+        this.LEFT_KEY = k;
+    }
+
+    public void changeRightKey(){
+        KeyCode k = null;
+        while (k == null){ k = InputManager.getPressedKey();}
+
+        this.RIGHT_KEY = k;
+    }
+
+    public void changeShootKey(){
+        KeyCode k = null;
+        while (k == null){ k = InputManager.getPressedKey();}
+
+        this.SHOOTING_KEY = k;
+    }
+
+    public void changeSkill1Key(){
+        KeyCode k = null;
+        while (k == null){ k = InputManager.getPressedKey();}
+
+        this.SKILL1_KEY = k;
+    }
+
+    public void changeSkill2Key(){
+        KeyCode k = null;
+        while (k == null){ k = InputManager.getPressedKey();}
+
+        this.SKILL2_KEY = k;
+    }
+
+    public void changeSkill3Key(){
+        KeyCode k = null;
+        while (k == null){ k = InputManager.getPressedKey();}
+
+        this.SKILL3_KEY = k;
+    }
+
 }
